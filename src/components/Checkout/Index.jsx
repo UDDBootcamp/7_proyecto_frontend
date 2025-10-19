3dd002bae533936a4093a9fccfa87854e99becf5
marco-rubio-bustos <marco.rubio.bustos@gmail.com>
2025-10-19 07:24:23 -0300

    Renombro index.jsx a Index.jsx correctamente

--- src/components/Checkout/Index.jsx
+++ src/components/Checkout/Index.jsx
@@ -138,7 +138,6 @@ export default function Checkout() {
               );
             })}
           </ul>
-
           <div className="bg-gray-100 px-4 py-6 sm:p-6 lg:p-8">
             <div>
               <dl className="-my-4 text-sm ">
